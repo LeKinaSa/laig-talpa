f40caafec5ecb7916846deee64c889a8e148aebc
Beatriz Mendes <up201806551@fe.up.pt>
2020-10-06 18:45:49 +0100

    created myleaf and mynode classes

---
+++ TP1/MyLeaf.js
@@ -0,0 +1,29 @@
+/**
+ * MyLeaf class: represents the possible primitives (leaves)
+ * @constructor
+**/
+class MyLeaf {
+
+    constructor(graph, element) {
+        this.graph = graph;
+        this.primitive;
+
+        // gets the type of the primitive from the xml file
+        var type = this.graph.reader.getItem(element, 'type', ['rectangle']);
+
+        // switch to decide what primitive is going to be shown in the screen
+        switch(type){
+            case 'rectangle':
+                this.x1 = this.graph.reader.getString(element, 'x1');
+                this.y1 = this.graph.reader.getString(element, 'y1');
+                this.x2 = this.graph.reader.getString(element, 'x2');
+                this.y2 = this.graph.reader.getString(element, 'y2');
+                this.primitive = new MyRectangle(this.graph.scene, this.x1, this.y1, this.x2, this.y2);
+                break;
+            default:
+                console.log("Not implemented yet");
+                break;
+        }
+
+    }
+}
---
+++ TP1/MyNode.js
@@ -0,0 +1,29 @@
+/**
+ * MyNode class: represents a node from the graph
+ * @constructor
+**/
+class MyNode {
+
+    constructor(graph, nodeID) {
+        this.graph = graph;
+        this.nodeID = nodeID;
+        this.children = [];
+        this.leaves = [];
+
+    /*  this.material = null;
+        this.texture = null;
+
+        this.matrix = mat4.create();
+        mat4.identity(this.matrix); */
+    }
+
+    // add a child identified by nodeID
+    addChild(nodeID) {
+        this.children.push(nodeID);
+    }
+
+    // add a child identified by leaf
+    addChild(leaf){
+        this.leaves.push(leaf);
+    }
+}
