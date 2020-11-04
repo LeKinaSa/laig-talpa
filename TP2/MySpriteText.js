34da1203b6bd6f455326eeed7376377b2cc740bc
Clara Martins <up201806528@fe.up.pt>
2020-11-04 16:45:35 +0000

    Font

--- TP2/MySpriteText.js
+++ TP2/MySpriteText.js
@@ -12,14 +12,14 @@ class MySpriteText {
     constructor(scene, text) {
         this.scene = scene;
         this.text = text;
-        this.spriteSheet = new MySpriteSheet(this.scene, new CGFtexture("./scenes/images/font.png"), 10, 9);
+        this.spriteSheet = new MySpriteSheet(this.scene, new CGFtexture("./scenes/images/font.png"), 10, 10);
         this.plane = new MyRectangle(scene, 0, 0, 1, 1);
     }
 
     display() {
         this.scene.pushMatrix();
 
-        this.activateShader();
+        this.spriteSheet.activateShader();
         this.scene.translate(- this.text.length / 2, -0.5, 0);
         var p;
         for (let index = 0; index < this.text.length; index ++) {
@@ -27,13 +27,13 @@ class MySpriteText {
             p = this.getCharacterPosition(index);
 
             // Activate Sprite
-            this.activateCellP(p);
+            this.spriteSheet.activateCellP(p);
 
             // Display Base Geometry
             if (i > 0) this.scene.translate(1, 0, 0);
             this.plane.display();
         }
-        this.deactivateShader();
+        this.spriteSheet.deactivateShader();
         
         this.scene.popMatrix();
     }
@@ -43,17 +43,9 @@ class MySpriteText {
         var charCode = this.text.charCodeAt(index);
         
         // Get Position in the SpriteSheet
-                // charCode == 46      - .
-                // 48 < charCode < 57  - 0 9
-                // 65 < charCode < 90  - A Z
-                // 97 < charCode < 122 - a z
-        if ((charCode >= 33) && (charCode <= 122)) {
-            return charCode - 33;
+        if ((charCode >= 32) && (charCode <= 126)) {
+            return charCode - 32;
         }
-        // depois podemos acrescentar o ' ' (32)
-        // { | } ~ a seguir ao z (123, 124, 125, 126)
-        // e fazer a textura ser 10x10
-        // 32 - 126
-        return -1;
+        return 0;
     }
 }
