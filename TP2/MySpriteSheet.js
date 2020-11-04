8ffaf4bd9bd94dc6c9ea31f3bb78d80cfd4fd544
Clara Martins <50105043+CAMartins@users.noreply.github.com>
2020-11-04 12:31:49 +0000

    Shaders and Functions to deal with them

--- TP2/MySpriteSheet.js
+++ TP2/MySpriteSheet.js
@@ -12,9 +12,34 @@ class MySpriteSheet {
      * @param {int} sizeN - Number of Lines
      */
     constructor(scene, texture, sizeM, sizeN) {
-        super(scene);
+        this.scene = scene;
         this.texture = texture;
         this.columns = sizeM;
         this.lines   = sizeN;
+        this.shader  = new CGFshader(this.scene.gl, "spritesheet.vert", "spritesheet.frag");
+        this.shader.setUniformsValues({ dimensions: [this.columns, this.lines] });
     }
+
+    activateShader() {
+        this.setActiveShader(this.shader);
+    }
+
+    activateCellP(p) {
+        // Get Character Sprite Position
+        // TODO : no idea
+        let column = 0;
+        let line   = 0;
+        
+        // Activate Shader
+        this.activateCellMN(column, line);
+    }
+
+    activateCellMN(column, line) {
+        this.shader.setUniformsValues({ charCoords: [column, line] });
+    }
+
+    deactivateShader() {
+        this.setActiveShader(this.scene.defaultShader);
+    }
+
 }
