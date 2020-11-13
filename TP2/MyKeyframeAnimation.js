8fd3290aff206c6e5d834c55712043452e2d1a0b
Clara Martins <up201806528@fe.up.pt>
2020-11-13 11:12:53 +0000

    Uses both frames and interpolation to calculate Position
    The only problem is now the mat4.rotate( ) since it's receiving an array and should be receiving an axis and an angle.

--- TP2/MyKeyframeAnimation.js
+++ TP2/MyKeyframeAnimation.js
@@ -11,8 +11,6 @@ class MyKeyframeAnimation extends MyAnimation {
         this.addKeyframe(new MyKeyframe(scene, 0, [0,0,0], [0,0,0], [1,1,1]));
         this.currentState = mat4.create();
         this.init = 0;
-        this.previous = 0;
-        this.final = 0;
     }
 
     // Adds a keyframe to the keyframes list
@@ -20,8 +18,6 @@ class MyKeyframeAnimation extends MyAnimation {
         // Keyframe class - aux class to create the Keyframes
         this.keyframes.push(keyframe);
         this.keyframes.sort((a, b) => (a.instant > b.instant) ? 1 : -1);
-        this.final=this.keyframes[this.keyframes.length-1].instant;
-        console.log(this.final);
     }
 
     
@@ -30,68 +26,85 @@ class MyKeyframeAnimation extends MyAnimation {
         // verify if it's the first call -> if it's the first, change init to current time
         if (this.init == 0) {
             this.init = t;
-            this.previous = t;
         }
 
-        /*
-        * delta_time -> animation's elapsed time
-        * elapsed time = actual time - init time
-        * for example: first call -> deltaTime = 0
-        */
+        /**
+         * delta_time -> animation's elapsed time
+         * elapsed time = actual time - init time
+         * for example: first call -> deltaTime = 0
+         */
         var delta_time = t - this.init;
 
-        /*
-        *  go through all frames; if the keyframe's instant is > than elapsed time, treat the
-        *  keyframe
-        */        
-        var aux = -1;
+        
+        /**
+         * the delta_time needs to be between the initial and the final instants of the animation
+         */
+        if ((delta_time < this.keyframes[0].instant)
+        || (delta_time > this.keyframes[this.keyframes.length-1].instant)) {
+           return;
+        }
+
+        /**
+         * identify the indexes from the frames needed to represent the object on the screen
+         * if delta_time matches one of the frame instants, then the position is the same as the frame
+         * if it doesn't, then we look for the frames exactly before and after the delta_time
+         */
+        var kf1_index, kf2_index, kf_index = -1;
         for (var i = 0; i < this.keyframes.length; ++ i) {
-            if (this.keyframes[i].instant > delta_time) {
-                aux = i;
+            if (this.keyframes[i].instant == delta_time) {
+                kf_index = i;
+                break;
+            }
+            else if (this.keyframes[i].instant > delta_time) {
+                kf2_index = i;
+                kf1_index = i - 1;
                 break;
             }
         }
 
-        /*
-        *  if there's no keyframe with instant bigger than elapsed time, no keyframe should
-        *  be processed
-        */
-        if (aux == -1) return;
+        var T = [0, 0, 0];
+        var R = [0, 0, 0];
+        var S = [1, 1, 1];
 
-        // else -> iterpolations
-        var kf1 = this.keyframes[aux - 1];
-        var kf2 = this.keyframes[aux];
+        if (kf_index != -1) {
+            var kf = this.keyframes[kf_index];
+            T = kf.translation;
+            R = kf.rotation * DEGREE_TO_RAD;
+            S = kf.scale;
+        }
+        else {
+            // Interpolations
+            var kf1 = this.keyframes[kf1_index];
+            var kf2 = this.keyframes[kf2_index];
+            
+            /**
+             *                    INTERPOLATION (BETWEEN 2 KEYFRAMES)
+             *                     X = Xi + XTtotal * Telapsed/Ttotal
+             * X:        current value
+             * Xi:       initial value
+             * XTotal:   value between initial/end time
+             * Telapsed: time from start until now
+             * Ttotal:   total animation time
+             */
 
-        /*
-        *                               INTERPOLATION
-        *                     X = Xi + XTtotal * Telapsed/Ttotal
-        * X:        current value
-        * Xi:       initial value
-        * XTotal:   value between initial/end time
-        * Telapsed: time from start until now
-        * Ttotal:   total animation time
-        */
-        var T = [0,0,0];
-        var R = [0,0,0];
-        var S = [1,1,1];
+            var elapsedTime = delta_time  - kf1.instant;
+            var totalTime   = kf2.instant - kf1.instant;
 
-        // Translation
-        T = vec3.lerp(T, kf1.translation, kf2.translation, (delta_time - kf1.instant)/(kf2.instant-kf1.instant));
-       
-        // Rotation
-        R = vec3.lerp(R, kf1.rotation * DEGREE_TO_RAD, kf2.rotation * DEGREE_TO_RAD, (delta_time - kf1.instant)/(kf2.instant-kf1.instant));
-
-        // Scale
-        S = vec3.lerp(S, kf1.scale, kf2.scale, (delta_time - kf1.instant)/(kf2.instant-kf1.instant));
+            // Translation
+            T = vec3.lerp(T, kf1.translation, kf2.translation, elapsedTime/totalTime);
         
+            // Rotation
+            R = vec3.lerp(R, kf1.rotation * DEGREE_TO_RAD, kf2.rotation * DEGREE_TO_RAD, elapsedTime/totalTime);
+
+            // Scale
+            S = vec3.lerp(S, kf1.scale, kf2.scale, elapsedTime/totalTime);    
+        }
+
         // Update current state
         this.currentState = mat4.create();
         mat4.translate(this.currentState, this.currentState, ...T);
-        mat4.rotate(this.currentState, this.currentState, ...R);
-        mat4.scale(this.currentState, this.currentState, ...S);
-
-        // previous = last time update(t) was called
-        this.previous = t;
+        mat4.rotate   (this.currentState, this.currentState, ...R);
+        mat4.scale    (this.currentState, this.currentState, ...S);
     }
 
     apply() {
