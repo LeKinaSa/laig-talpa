2bd0c1dd57db26cd43cecc8b02e01d4d23171a9b
Clara Martins <up201806528@fe.up.pt>
2020-11-18 12:41:04 +0000

    Testing MyDefBarrel (constants)

--- TP2/primitives/MyDefBarrel.js
+++ TP2/primitives/MyDefBarrel.js
@@ -16,13 +16,37 @@ class MyDefBarrel extends CGFobject{
         var L = this.height;
         var alpha = Math.PI / 6; // 30graus
         var controlvertexes = [
-            [r, 0, 0],
-            [r + H, 0, H / Math.tan(alpha)],
-            [r + H, 0, L - H / Math.tan(alpha)],
-            [r, 0, L]
+            [
+                [    r, 0.0, 0],
+                [r + H, 0.0, H / Math.tan(alpha)],
+                [r + H, 0.0, L - H / Math.tan(alpha)],
+                [    r, 0.0, L]
+            ],
+            [
+                [r    , 0.5, 0],
+                [r + H, 0.5, H / Math.tan(alpha)],
+                [r + H, 0.5, L - H / Math.tan(alpha)],
+                [r    , 0.5, L]
+            ],
+            [
+                [    r, 1.0, 0],
+                [r + H, 1.0, H / Math.tan(alpha)],
+                [r + H, 1.0, L - H / Math.tan(alpha)],
+                [    r, 1.0, L]
+            ],
+            [
+                [    r, 1.5, 0],
+                [r + H, 1.5, H / Math.tan(alpha)],
+                [r + H, 1.5, L - H / Math.tan(alpha)],
+                [    r, 1.5, L]
+            ]
         ];
         var nurbsSurface = new CGFnurbsSurface(this.slices - 1, this.stacks - 1, controlvertexes);
         this.obj = new CGFnurbsObject(this.scene, this.slices, this.stacks, nurbsSurface)
+    }
+
+    display() {
+        this.obj.display();
     }
 
     /**
@@ -30,5 +54,5 @@ class MyDefBarrel extends CGFobject{
      * @param {float} afs - Amplification Factor on S 
      * @param {float} aft - Amplification Factor on T
      */
-	updateTexCoords(afs, aft) { }
+    updateTexCoords(afs, aft) { }
 }
