9aab6acf33ddcab1e04228b81264e9503f9b287f
Beatriz Mendes <up201806551@fe.up.pt>
2020-11-25 12:21:06 +0000

    Create MyGameboard.js

---
+++ TP3/MyGameboard.js
@@ -0,0 +1,35 @@
+class MyGameboard extends CGFobject {
+    constructor(scene, board){
+        super(scene);
+        this.board = board;
+    }
+
+    addPiece(tile){
+
+    }
+
+    removePiece(tile){
+
+    }
+
+    getPiece(tile){
+        // return piece;
+    }
+
+    getTile(piece){
+        // return tile;
+    }
+
+    getTile(x, y){
+        // return tile;
+    }
+
+    movePiece(piece, startingTile, destinationTile){
+
+    }
+
+    display(){
+        // tile.display()
+        // piece.display()
+    }
+}
