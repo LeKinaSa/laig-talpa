c891934c03c41e7a1f4473b67f104c996096d857
Clara Martins <up201806528@fe.up.pt>
2020-12-02 11:07:40 +0000

    Documentation for MyGameBoard

--- TP3/gameboard/MyGameboard.js
+++ TP3/gameboard/MyGameboard.js
@@ -30,16 +30,25 @@ class MyGameBoard {
 
     /**
      * Remove the Piece Standing on this Tile
-     * @param {MyTile} tile - Tile where the Piece will be Removed
+     * @param {MyTile} tile - Tile where the Piece is Standing
      */
     removePiece(tile) {
         tile.unsetPiece(this);
     }
 
+    /**
+     * Get the Piece Standing on this Tile
+     * @param {MyTile} tile - Tile where the Piece is Standing
+     */
     getPiece(tile) {
         tile.getPiece();
     }
 
+    /**
+     * Get the Tile in this Column and Line of the GameBoard
+     * @param {int} column
+     * @param {int} line
+     */
     getTile(column, line) {
         var x = column - 1;
         var y = 8 - line;
@@ -48,11 +57,19 @@ class MyGameBoard {
         return tile;
     }
 
+    /**
+     * Move the Piece from Starting Tile to Destination Tile
+     * @param {MyTile} startingTile - Starting Tile
+     * @param {MyTile} destinationTile - Destination Tile
+     */
     movePiece(startingTile, destinationTile) {
         var piece = this.getPiece(startingTile);
         destinationTile.setPiece(this, piece);
     }
 
+    /**
+     * Display GameBoard
+     */
     display() {
         for (var i = 0; i < this.tiles.length; ++ i) {
             this.tiles[i].display();
