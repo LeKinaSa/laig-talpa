b6f8e4d82a705ea5214266da76becaabd19c764d
Beatriz Mendes <up201806551@fe.up.pt>
2020-12-09 11:52:53 +0000

    create new classes to help with animations

---
+++ TP3/gameboard/MyMoveAnimator.js
@@ -0,0 +1,6 @@
+/**
+ * MyMoveAnimator
+ */
+class MyMoveAnimator extends MyAnimator{
+
+}
--- TP3/main.js
+++ TP3/main.js
@@ -17,6 +17,7 @@ serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.j
 'animations/MySpriteSheet.js', 'animations/MySpriteText.js', 'animations/MySpriteAnim.js',
 'primitives/MyPlane.js', 'primitives/MyPatch.js', 'primitives/MyDefBarrel.js',
 'gameboard/MyPiece.js', 'gameboard/MyTile.js', 'gameboard/MyGameBoard.js', 'MyPrologConnection.js',
+'gameboard/MyAnimator.js', 'gameboard/MyGameOrchestrator.js', 'gameboard/MyMoveAnimator.js',
 
 main=function()
 {
