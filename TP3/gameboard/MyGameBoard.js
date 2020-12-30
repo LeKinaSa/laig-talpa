450b5c142f920774912887276bef4f85819454a7
Clara Martins <up201806528@fe.up.pt>
2020-12-30 22:53:23 +0000

    Remove Comments

--- TP3/gameboard/MyGameBoard.js
+++ TP3/gameboard/MyGameBoard.js
@@ -102,10 +102,8 @@ class MyGameBoard {
     }
     
     toJS(prologBoard) {
-        console.log(prologBoard);
-        console.log(prologBoard.length, prologBoard[0].length);
         for (let line = 0; line < prologBoard.length; ++ line) {
-            for (let column = 0; column < prologBoard[line].length; ++ column) {  
+            for (let column = 0; column < prologBoard[line].length; ++ column) {
                 let tile = new MyTile(this.scene, [column + 1, line + 1], this.tiletexture);
                 if (prologBoard[line][column] == "O") {       // blue
                     tile.piece = new MyPiece(this.scene, 'blue');
